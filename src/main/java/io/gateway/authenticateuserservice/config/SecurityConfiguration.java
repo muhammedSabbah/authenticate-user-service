46176e9b25b01cb85660bda7fe50d9fa36311d74
Muhammed Sabbah <30741844+muhammedSabbah@users.noreply.github.com>
2022-10-12 13:33:11 +0200

    Update SecurityConfiguration.java

--- src/main/java/io/gateway/authenticateuserservice/config/SecurityConfiguration.java
+++ src/main/java/io/gateway/authenticateuserservice/config/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package io.gateway.authenticateuserservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +13,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import io.gateway.authenticateuserservice.utils.USER_ROLE;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +25,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+	
+	@Value("${app.version}")
+	private String appVersion;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -29,10 +36,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		GatewayAuthFilter filter = new GatewayAuthFilter(authenticationManagerBean());
+		filter.setFilterProcessesUrl("/api/v1.0/login");
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
-		http.addFilter(new GatewayAuthFilter(authenticationManagerBean()));
+		http.authorizeRequests().antMatchers("/api/v1.0/login/**").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1.0/users/**").hasAnyAuthority(USER_ROLE.ROLE_ADMIN.value());
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/v1.0/user/save/**").hasAnyAuthority(USER_ROLE.ROLE_SUPER_ADMIN.value());
+		http.authorizeRequests().anyRequest().authenticated();
+		http.addFilter(filter);
 	}
 
 	@Bean
